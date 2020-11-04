6bb7363f00d3619b5473faed6b1bab0ca497b1e2
Gaurav Thakur <gthakur581@gmail.com>
2020-11-04 08:57:07 +0530

    Now you can tap on any list title in order to open it on startup. otherwise default behavior will be applied

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,27 +1,31 @@
-import React, {useContext} from "react";
-import {Typography} from "@material-ui/core";
-import {makeStyles} from "@material-ui/core/styles";
-import {ThemeContext} from "../context/ThemeProvider";
+import React, { useContext } from "react";
+import { Typography } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import { ThemeContext } from "../context/ThemeProvider";
 
-
-const Title = ({listName}) => {
-    const data = useContext(ThemeContext)
-    const useStyle = makeStyles((theme) => ({
-        title: {
-            color: data.theme === 'dark' ? 'white' : 'teal',
-            margin: '20px 0 20px 0',
-            fontFamily: ['Fredoka One', 'cursive'],
-            [theme.breakpoints.down('md')]: {
-                fontSize: '2em'
-            },
-        }
-    }))
-    const classes = useStyle();
-    return (
-        <Typography className={classes.title} align='center' variant='h3'>
-            {listName}
-        </Typography>
-    )
-}
+const Title = ({ listName }) => {
+  const data = useContext(ThemeContext);
+  const useStyle = makeStyles((theme) => ({
+    title: {
+      color: data.theme === "dark" ? "white" : "teal",
+      margin: "20px 0 20px 0",
+      fontFamily: ["Fredoka One", "cursive"],
+      [theme.breakpoints.down("md")]: {
+        fontSize: "2em",
+      },
+    },
+  }));
+  const classes = useStyle();
+  return (
+    <Typography
+      onClick={() => localStorage.setItem("listName", listName)}
+      className={classes.title}
+      align="center"
+      variant="h3"
+    >
+      {listName}
+    </Typography>
+  );
+};
 
 export default Title;
