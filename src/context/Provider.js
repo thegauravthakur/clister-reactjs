cf0a5707627a67e4d53b0c23bfadaeb8f12768fe
Gaurav Thakur <gthakur581@gmail.com>
2020-11-05 16:05:07 +0530

    updated the manifest.json

--- src/context/Provider.js
+++ src/context/Provider.js
@@ -1,44 +1,43 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, {useContext, useEffect, useState} from "react";
 import app from "../firebase/base";
 import LinearProgress from "@material-ui/core/LinearProgress";
-import { ThemeContext } from "./ThemeProvider";
-import { useLocation } from "react-router-dom";
+import {ThemeContext} from "./ThemeProvider";
+import {useLocation} from "react-router-dom";
+
 export const AuthContext = React.createContext("");
-export const AuthProvider = ({ children }) => {
-  const location = useLocation();
-  const currentTheme = useContext(ThemeContext);
-  const [currentUser, setCurrentUser] = useState(null);
-  const [pending, setPending] = useState(true);
-  useEffect(() => {
-    app.auth().onAuthStateChanged((user) => {
-      setCurrentUser(user);
-      setPending(false);
-    });
-  }, []);
-  if (pending) {
+export const AuthProvider = ({children}) => {
+    const location = useLocation();
+    const currentTheme = useContext(ThemeContext);
+    const [currentUser, setCurrentUser] = useState(null);
+    const [pending, setPending] = useState(true);
+    useEffect(() => {
+        app.auth().onAuthStateChanged((user) => {
+            setCurrentUser(user);
+            setPending(false);
+        });
+    }, []);
+    if (pending) {
+        return (
+            <div
+                style={{
+                    backgroundColor:
+                        currentTheme.theme === "dark"
+                            ? "black"
+                            : "white",
+                    minHeight: "100vh",
+                }}
+            >
+                <LinearProgress color="secondary"/>
+            </div>
+        );
+    }
     return (
-      <div
-        style={{
-          backgroundColor:
-            location.pathname === "/"
-              ? "black"
-              : currentTheme.theme === "dark"
-              ? "black"
-              : "white",
-          minHeight: "100vh",
-        }}
-      >
-        <LinearProgress color="secondary" />
-      </div>
+        <AuthContext.Provider
+            value={{
+                currentUser,
+            }}
+        >
+            {children}
+        </AuthContext.Provider>
     );
-  }
-  return (
-    <AuthContext.Provider
-      value={{
-        currentUser,
-      }}
-    >
-      {children}
-    </AuthContext.Provider>
-  );
 };
