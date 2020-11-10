1a64b1142fd68304a7a8ea9c8bca027a35454cfa
Gaurav Thakur <gthakur581@gmail.com>
2020-11-10 20:05:31 +0530

    updated the readme file

--- src/context/Provider.js
+++ src/context/Provider.js
@@ -1,43 +1,38 @@
-import React, {useContext, useEffect, useState} from "react";
+import React, { useContext, useEffect, useState } from "react";
 import app from "../firebase/base";
 import LinearProgress from "@material-ui/core/LinearProgress";
-import {ThemeContext} from "./ThemeProvider";
-import {useLocation} from "react-router-dom";
+import { ThemeContext } from "./ThemeProvider";
 
 export const AuthContext = React.createContext("");
-export const AuthProvider = ({children}) => {
-    const location = useLocation();
-    const currentTheme = useContext(ThemeContext);
-    const [currentUser, setCurrentUser] = useState(null);
-    const [pending, setPending] = useState(true);
-    useEffect(() => {
-        app.auth().onAuthStateChanged((user) => {
-            setCurrentUser(user);
-            setPending(false);
-        });
-    }, []);
-    if (pending) {
-        return (
-            <div
-                style={{
-                    backgroundColor:
-                        currentTheme.theme === "dark"
-                            ? "black"
-                            : "white",
-                    minHeight: "100vh",
-                }}
-            >
-                <LinearProgress color="secondary"/>
-            </div>
-        );
-    }
+export const AuthProvider = ({ children }) => {
+  const currentTheme = useContext(ThemeContext);
+  const [currentUser, setCurrentUser] = useState(null);
+  const [pending, setPending] = useState(true);
+  useEffect(() => {
+    app.auth().onAuthStateChanged((user) => {
+      setCurrentUser(user);
+      setPending(false);
+    });
+  }, []);
+  if (pending) {
     return (
-        <AuthContext.Provider
-            value={{
-                currentUser,
-            }}
-        >
-            {children}
-        </AuthContext.Provider>
+      <div
+        style={{
+          backgroundColor: currentTheme.theme === "dark" ? "black" : "white",
+          minHeight: "100vh",
+        }}
+      >
+        <LinearProgress color="secondary" />
+      </div>
     );
+  }
+  return (
+    <AuthContext.Provider
+      value={{
+        currentUser,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
 };
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,9 +1,8 @@
-import React, { useContext, useEffect } from "react";
+import React from "react";
 import { createMuiTheme, Paper, ThemeProvider } from "@material-ui/core";
 import Header from "../components/Header";
 import FeaturesSection from "../components/FeaturesSection";
 import FeaturesSection2 from "../components/FeatureSection2";
-import { ThemeContext } from "../context/ThemeProvider";
 import FeaturesSection3 from "../components/FeatureSection3";
 import Footer from "../components/Footer";
 import EndSection from "../components/EndSection";
